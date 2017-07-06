8984abbf9071357f1145b4d43e9d83872acc0257
Curtis Xiao <xiao.curtis@gmail.com>
2017-07-05 18:49:48 -0600

    Add db connection and load model + router

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 let express = require('express'),
   app = express(),
-  port = process.env.PORT || 10086;
+  port = process.env.PORT || 10086,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/nodeModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect = ('mongodb://localhost/Nodedb')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/nodeRoutes');
+routes(app);
 
 app.listen(port);
 
